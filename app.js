0a34d54d3ac952447c8abcd7f8454bca728d204a
Muhammad Ismail <connect.ismail@protonmail.com>
2022-07-31 14:51:39 +0530

    basic auth

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require(`express`);
 const cors = require(`cors`);
+const path = require('path');
 const bodyparser = require('body-parser');  
 const authRouter = require(`./src/routes/auth-router`);
 const booksRouter = require(`./src/routes/book-router`);
@@ -10,14 +11,17 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static('./dist/library-app-frontend'));
 
 app.use(`/api/auth`, authRouter);
 app.use(`/api/books`, booksRouter);
 
+// Server
 const PORT = process.env.PORT || 4444;
 app.listen(PORT, ()=> {
   console.log(`Hi, I'm listening at ${PORT}`);
 });
-app.get(`/`, (req, res)=> {
-  res.send(`Hi I'm listening at /`);
+
+app.get('/*', (req, res)=> {   //For hosting
+  res.sendFile(path.join(__dirname + './dist/library-app-frontend/index.html'));
 });
